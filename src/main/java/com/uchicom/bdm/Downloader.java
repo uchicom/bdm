627574da80ecffec2f97bf922143ed571849fed0
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2019-05-04 15:28:57 +0900

    ログ出力追加

--- src/main/java/com/uchicom/bdm/Downloader.java
+++ src/main/java/com/uchicom/bdm/Downloader.java
@@ -17,20 +17,40 @@ public class Downloader {
 
 	List<String> urlList;
 	File outputDir;
+
 	public Downloader(List<String> urlList, File outputDir) {
 		this.urlList = urlList;
 		this.outputDir = outputDir;
 	}
+	boolean alive;
 	public void execute() {
 		for (String urlString : urlList) {
 			try {
 				URL url = new URL(urlString);
-				System.out.println(urlString.substring(urlString.lastIndexOf('/') + 1));
+				System.out.print(urlString.substring(urlString.lastIndexOf('/') + 1));
+				System.out.print(" ");
+				alive = true;
+				Thread thread = new Thread(() -> {
+					while (alive) {
+						System.out.print("-");
+						try {
+							Thread.sleep(10_000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				thread.start();
 				try (InputStream stream = url.openStream()) {
-					Files.copy(stream, new File(outputDir,urlString.substring(urlString.lastIndexOf('/') + 1)).toPath());
+					Files.copy(stream,
+							new File(outputDir, urlString.substring(urlString.lastIndexOf('/') + 1)).toPath());
+					alive = false;
+					System.out.println("> OK");
 				} catch (IOException e) {
 					// TODO 自動生成された catch ブロック
 					e.printStackTrace();
+					System.out.println("> NG");
 				}
 			} catch (MalformedURLException e) {
 				// TODO 自動生成された catch ブロック
