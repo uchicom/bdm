0712e07f5c5bd3cff26ff6b8cb973355c717c55d
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2021-06-04 16:14:31 +0900

    削除追加

--- src/main/java/com/uchicom/bdm/domain/S3.java
+++ src/main/java/com/uchicom/bdm/domain/S3.java
@@ -34,122 +34,146 @@ import com.amazonaws.services.s3.model.StaticEncryptionMaterialsProvider;
  */
 public class S3 {
 
-	public static void main(String[] args) {
-		if (args.length > 2) {
-			File file = new File(args[0]);
-			if (!file.exists()) {
-				System.out.println(args[0] + " file is not exist!");
-				return;
-			}
-			S3 s3 = new S3(file);
-			switch (args[1]) {
-			case "list":
-				s3.list(Arrays.copyOfRange(args, 2, args.length));
-				break;
-			case "download":
-				s3.download(Arrays.copyOfRange(args, 2, args.length));
-				break;
-			default:
-				System.out.println("list or download");
-			}
+    public static void main(String[] args) {
+        if (args.length > 2) {
+            File file = new File(args[0]);
+            if (!file.exists()) {
+                System.out.println(args[0] + " file is not exist!");
+                return;
+            }
+            S3 s3 = new S3(file);
+            switch (args[1]) {
+            case "list":
+                s3.list(Arrays.copyOfRange(args, 2, args.length));
+                break;
+            case "download":
+                s3.download(Arrays.copyOfRange(args, 2, args.length));
+                break;
+            case "delete":
+                s3.delete(Arrays.copyOfRange(args, 2, args.length));
+                break;
+            default:
+                System.out.println("list or download");
+            }
 
-		}
+        }
 
-	}
+    }
 
-	private Properties properties = new Properties();
+    private Properties properties = new Properties();
 
-	public S3(File file) {
-		try (FileInputStream fis = new FileInputStream(file)) {
-			properties.load(fis);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
+    public S3(File file) {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * パス配下のリストをファイル出力.
-	 * 
-	 * @param keys オブジェクトキーの配列
-	 */
-	public void list(String[] keys) {
-		AmazonS3 client = createClient(false);
-		String bucketName = properties.getProperty("bucket_name");
-		for (String key : keys) {
-			ListObjectsV2Result result = client.listObjectsV2(bucketName, key);
-			result.getObjectSummaries().forEach(sum -> System.out.println(sum.getKey()));
-		}
-	}
+    /**
+     * パス配下のリストをファイル出力.
+     * 
+     * @param keys オブジェクトキーの配列
+     */
+    public void list(String[] keys) {
+        AmazonS3 client = createClient(false);
+        String bucketName = properties.getProperty("bucket_name");
+        for (String key : keys) {
+            ListObjectsV2Result result = client.listObjectsV2(bucketName, key);
+            result.getObjectSummaries().forEach(sum -> System.out.println(sum.getKey()));
+        }
+    }
 
-	/**
-	 * ダウンロードする
-	 * 
-	 * @param keys オブジェクトキーの配列
-	 */
-	public void download(String[] keys) {
-		AmazonS3 client = createClient(true);
-		String bucketName = properties.getProperty("bucket_name");
-		for (String key : keys) {
-			S3Object object = client.getObject(new GetObjectRequest(bucketName, key));
-			byte[] bytes = new byte[4 * 1024 * 1024]; // 4M
-			try (S3ObjectInputStream oc = object.getObjectContent(); FileOutputStream fos = new FileOutputStream(key)) {
-				int length = 0;
-				while ((length = oc.read(bytes)) != -1) {
-					fos.write(bytes, 0, length);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    /**
+     * ダウンロードする
+     * 
+     * @param keys オブジェクトキーの配列
+     */
+    public void download(String[] keys) {
+        AmazonS3 client = createClient(true);
+        String bucketName = properties.getProperty("bucket_name");
+        for (String key : keys) {
+            S3Object object = client.getObject(new GetObjectRequest(bucketName, key));
+            byte[] bytes = new byte[4 * 1024 * 1024]; // 4M
+            File keyFile = new File(key);
+            File parent = keyFile.getParentFile();
+            if (!parent.exists()) {
+                parent.mkdirs();
+            }
+            try (S3ObjectInputStream oc = object.getObjectContent();
+                    FileOutputStream fos = new FileOutputStream(keyFile)) {
+                int length = 0;
+                while ((length = oc.read(bytes)) != -1) {
+                    fos.write(bytes, 0, length);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public AmazonS3 createClient(boolean encryptable) {
-		AmazonS3EncryptionClientV2Builder builder = AmazonS3EncryptionClientV2Builder.standard();
+    /**
+     * 削除する
+     * 
+     * @param keys オブジェクトキーの配列
+     */
+    public void delete(String[] keys) {
+        AmazonS3 client = createClient(false);
+        String bucketName = properties.getProperty("bucket_name");
+        for (String key : keys) {
+            client.deleteObject(bucketName, key);
+        }
+    }
 
-		builder.withClientConfiguration(createConfiguration()).withCredentials(createCredentials())
-				.withRegion(createRegion());
-		if (encryptable && properties.getProperty("encrypt") != null) {
-			builder.withCryptoConfiguration(createCryptoConfiguration())
-					.withEncryptionMaterialsProvider(createEncryptionMaterialsProvider());
-		}
-		return builder.build();
-	}
+    public AmazonS3 createClient(boolean encryptable) {
+        if (encryptable && properties.getProperty("encrypt") != null) {
+            return AmazonS3EncryptionClientV2Builder.standard().withClientConfiguration(createConfiguration())
+                    .withCredentials(createCredentials()).withRegion(createRegion())
+                    .withCryptoConfiguration(createCryptoConfiguration())
+                    .withEncryptionMaterialsProvider(createEncryptionMaterialsProvider()).withRegion(createRegion())
+                    .build();
+        } else {
+            return AmazonS3ClientBuilder.standard().withClientConfiguration(createConfiguration())
+                    .withCredentials(createCredentials()).withRegion(createRegion()).build();
+        }
+    }
 
-	public ClientConfiguration createConfiguration() {
-		ClientConfiguration configuration = new ClientConfiguration();
-		configuration.setConnectionTimeout(30_000);
-		configuration.setMaxErrorRetry(3);
-		configuration.setRequestTimeout(30_000);
-		return configuration;
-	}
+    public ClientConfiguration createConfiguration() {
+        ClientConfiguration configuration = new ClientConfiguration();
+        configuration.setConnectionTimeout(30_000);
+        configuration.setMaxErrorRetry(3);
+        configuration.setRequestTimeout(30_000);
+        return configuration;
+    }
 
-	public AWSCredentialsProvider createCredentials() {
-		return new AWSStaticCredentialsProvider(
-				new BasicAWSCredentials(properties.getProperty("access_key"), properties.getProperty("secret_key")));
-	}
+    public AWSCredentialsProvider createCredentials() {
+        return new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(properties.getProperty("access_key"), properties.getProperty("secret_key")));
+    }
 
-	public Regions createRegion() {
-		return Regions.valueOf(properties.getProperty("region"));
-	}
+    public Regions createRegion() {
+        return Regions.valueOf(properties.getProperty("region"));
+    }
 
-	public AmazonS3 createClientWithEncrypt() {
-		ClientConfiguration configuration = new ClientConfiguration();
-		configuration.setConnectionTimeout(30_000);
-		configuration.setMaxErrorRetry(3);
-		configuration.setRequestTimeout(30_000);
-		return AmazonS3ClientBuilder.standard().withClientConfiguration(configuration)
-				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
-						properties.getProperty("access_key"), properties.getProperty("secret_key"))))
-				.withRegion(Regions.valueOf(properties.getProperty("region"))).build();
-	}
+    public AmazonS3 createClientWithEncrypt() {
+        ClientConfiguration configuration = new ClientConfiguration();
+        configuration.setConnectionTimeout(30_000);
+        configuration.setMaxErrorRetry(3);
+        configuration.setRequestTimeout(30_000);
+        return AmazonS3ClientBuilder.standard().withClientConfiguration(configuration)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                        properties.getProperty("access_key"), properties.getProperty("secret_key"))))
+                .withRegion(Regions.valueOf(properties.getProperty("region"))).build();
+    }
 
-	public CryptoConfigurationV2 createCryptoConfiguration() {
-		return new CryptoConfigurationV2().withCryptoMode(CryptoMode.AuthenticatedEncryption);
-	}
+    public CryptoConfigurationV2 createCryptoConfiguration() {
+        return new CryptoConfigurationV2().withCryptoMode(CryptoMode.AuthenticatedEncryption);
+    }
 
-	public StaticEncryptionMaterialsProvider createEncryptionMaterialsProvider() {
-		return new StaticEncryptionMaterialsProvider(new EncryptionMaterials(new SecretKeySpec(
-				Base64.getDecoder().decode(properties.getProperty("encrypt_key")), properties.getProperty("encrypt"))));
-	}
+    public StaticEncryptionMaterialsProvider createEncryptionMaterialsProvider() {
+        return new StaticEncryptionMaterialsProvider(new EncryptionMaterials(new SecretKeySpec(
+                Base64.getDecoder().decode(properties.getProperty("encrypt_key")), properties.getProperty("encrypt"))));
+    }
+
 }
